59fd71d4cc479f3161877a023449b9c5ddbc09c6
139mygit <13811329293@139.com>
2025-07-30 20:33:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5145,7 +5145,7 @@ async def get_original(input_data, org_text, file_name="", target_text=""):
                 if org_text[:4] in similar_content[:6]:
                     src_content = similar_content
                     break
-            elif file_name.startswith("180332"):
+            elif re.search("180332|180358|180359", file_name):
                 if org_text[:5] in similar_content[:10]:
                     if target_text == "セクター別配分":
                         re_content = re.search("(セクター別.*)個別の寄与度", similar_content, re.DOTALL)
